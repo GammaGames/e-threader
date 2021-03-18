504c8354bdeb018f779822e7841aac4b726a49b5
GammaGames <jesse007@ymail.com>
2021-03-18 13:41:22 -0600

    wip

--- e-threader/main.py
+++ e-threader/main.py
@@ -3,6 +3,8 @@ import os
 from pony.orm import db_session
 from models import Guild, Channel, Message, User, Thread
 
+SIGNUP_REACT = "🙋"
+CONFIRM_REACT = "✔️"
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
@@ -12,12 +14,36 @@ def print(*args, **kwargs):
     _print(*args, flush=True, **kwargs)
 
 
+def filter_user(user=None):
+    return user != client.user
+
+
+@db_session
+async def init_signup(u):
+    print("signup", u)
+    user = User[u.id]
+
+    confirm_message = await u.send("Confirm?")
+    # await confirm_message.add_reaction("✔️")
+    print(confirm_message)
+
+
+@db_session
+async def confirm_signup(user):
+    pass
+
+
+@db_session
+async def process_message(message):
+    pass
+
+
 @client.event
 async def on_ready():
     with db_session():
         global join_guild, join_channel, join_message
         print("We have logged in as {0.user}".format(client))
-        print(client.private_channels)
+        # print(client.private_channels)
         for g in client.guilds:
             guild = Guild[g.id] if Guild.exists(id=g.id) else Guild(id=g.id, name=g.name)
 
@@ -25,18 +51,31 @@ async def on_ready():
                 channel = Channel[c.id] if Channel.exists(id=c.id) else Channel(id=c.id, guild=guild, name=c.name)
                 if c.name == "sign-up":
                     if Message.exists(channel=channel):
-                        pass
+                        message = Message.get(channel=channel)
+                        m = await c.fetch_message(message.id)
+
+                        for reaction in m.reactions:
+                            print(reaction)
+                            async for u in reaction.users():
+                                if filter_user(u):
+                                    user = User[u.id] if User.exists(id=u.id) else User(id=u.id, name=u.name)
+                                    if not user.email:
+                                        print(f"init signup {u}")
+                                        z = await init_signup(u)
+
+
                         # TODO check if new subscribers
                     else:
                         m = await c.send("React to this message to set up a bot!")
                         message = Message(id=m.id, channel=channel, content=m.content)
-                        await m.add_reaction("🙋")
+                        await m.add_reaction(SIGNUP_REACT)
 
 
 @client.event
 async def on_raw_reaction_add(payload):
+    # print(payload)
     u = payload.member
-    if u != client.user:
+    if filter_user(u):
         with db_session():
             guild = Guild[payload.guild_id]
             channel = Channel[payload.channel_id]
